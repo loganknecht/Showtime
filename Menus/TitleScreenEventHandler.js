324d5b70c98f6eee3a309865208b908ef594be3a
unknown <Hugbot@Hugbot-PC.(none)>
2012-07-01 03:40:41 -0700

    updating new scripts, menu screen added

---
+++ Menus/TitleScreenEventHandler.js
@@ -0,0 +1,62 @@
+#pragma strict
+
+var titleScreenState : int = 0;
+var flashTimer : int = 0;
+var flashTimerMax : int = 10;
+var displayFlashingText : boolean = true;
+
+var gameTitleString : String = "";
+
+var gameTitle : GUIText;
+var flashingPressStartText : GUIText;
+
+function Start () {
+}
+
+function Update () {
+	processPlayerKeyInput();
+	manageTitleScreenText();
+}
+
+function processPlayerKeyInput() {
+	if(Input.GetKeyDown("return")) {
+		switch(titleScreenState) {
+			case(0):
+				titleScreenState = 1;
+				//Application.LoadLevel("Sandbox");
+			break;
+		}
+		Application.LoadLevel("Sandbox");
+	}
+	if(Input.GetKeyDown("escape")) {
+		switch(titleScreenState) {
+			case(0):
+			break;
+			case(1):
+				titleScreenState = 0;
+			break;
+		}
+	}
+}
+
+function manageTitleScreenText() {
+	switch(titleScreenState) {
+		case(0):
+			gameTitle.text = gameTitleString;
+			flashTimer++;
+			if(flashTimer > flashTimerMax) {
+				flashTimer = 0;
+				if(displayFlashingText) {
+					displayFlashingText = false;
+					flashingPressStartText.text = "";
+				}
+				else {
+					displayFlashingText = true;
+					flashingPressStartText.text =  "Press Start to Play";
+				}
+			}
+		break;
+		case(1):
+		break;
+	}
+}
